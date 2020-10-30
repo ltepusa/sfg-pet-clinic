b07c27fe81d3858b0e4af79738d9d9a884ccb253
ltepusa <loredana.tepusa@yahoo.com>
2020-10-30 17:15:44 +0200

    Fixed find by like. Closes #60

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -42,7 +42,7 @@ public class OwnerController {
         }
 
         //find owners by last name
-        List<Owner> results = ownerService.findAllByLastNameLike(owner.getLastName());
+        List<Owner> results = ownerService.findAllByLastNameLike("%" + owner.getLastName() + "%");
         if(results.isEmpty()) {
             //no owners find
             result.rejectValue("lastName", "not found", "not found");
